28a0bcffbb1a1f1200884e8094146494fba6c02b
Bruce <gabrielcp4@outlook.com>
2024-10-02 19:51:25 -0300

    preencher endereço pelo cep (incompleto)

---
+++ getAdressFromCep.js
@@ -0,0 +1,10 @@
+export default async function getAddressFromCep(cep) {
+    const url = "https://h-apigateway.conectagov.estaleiro.serpro.gov.br/api-cep/v1/consulta/cep/"
+    const response = await fetch(`${url}${cep}` , {
+        method : "GET",
+        headers: {
+            "x-cpf-usuario" : "70048110426"
+        }
+    })
+    return response
+}
